6a7927dbfbe26b6219a84a53a3387c4aa0a8a701
Nimay Malik <142222072+Nimaymalik@users.noreply.github.com>
2024-09-24 07:41:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+// Toggle navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+// Scroll section
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(`header nav a[href*=${id}]`)
+          .classList.add("active");
+      });
+    }
+  });
+
+  // Sticky navbar
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // Remove toggle icon and navbar
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
+};
+
+// Scroll reveal
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-contact h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-contact p, .about-content", { origin: "right" });
+
+const typed = new Typed(".multiple-text",{
+    strings:['Frontend Developer','Backend developer',"Blockchain developer"],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+});
